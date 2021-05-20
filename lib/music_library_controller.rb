d69a543ef06a7502f2b986d509d50e6d11fd8df2
Shasha Yan <shashayan@gmail.com>
2021-05-20 05:13:07 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -56,7 +56,7 @@ class MusicLibraryController
 
     def list_songs_by_artist
 
-      puts "Please enter the name of a artist:"
+      puts "Please enter the name of an artist:"
       input = gets.strip
 
       if artist = Artist.find_by_name(input)
